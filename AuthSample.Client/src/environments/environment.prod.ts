fb629da554d347bdd9b4131b588deabc75f959c0
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-09-29 22:55:31 +0200

    API url in configuration

--- AuthSample.Client/src/environments/environment.prod.ts
+++ AuthSample.Client/src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 import { AuthConfiguration } from '../app/shared/';
 
 export const environment = Object.assign({
-  production: true
+  production: true,
+  apiUrl: 'http://locahost:5000'
 }, {
   clientID: '1oy397EXD8DWvaJlhonAwRiBTgP9sXrD',
   domain: 'blazejewicz.eu.auth0.com'
--- AuthSample.Client/src/environments/environment.ts
+++ AuthSample.Client/src/environments/environment.ts
@@ -6,7 +6,8 @@
 import { AuthConfiguration } from '../app/shared/';
 
 export const environment = Object.assign({
-  production: false
+  production: false,
+  apiUrl: 'http://locahost:5000'
 }, {
   clientID: '1oy397EXD8DWvaJlhonAwRiBTgP9sXrD',
   domain: 'blazejewicz.eu.auth0.com'
