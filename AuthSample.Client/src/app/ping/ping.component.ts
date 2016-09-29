df6f526eaae3ee76a7fc845ff4884dd13deec934
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-09-29 22:55:22 +0200

    Ping view markup and logic

--- AuthSample.Client/src/app/ping/ping.component.ts
+++ AuthSample.Client/src/app/ping/ping.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+
+import { AuthHttp } from 'angular2-jwt';
+
+import { environment } from '../../environments/environment'
+import {
+  AuthService
+} from '../shared';
 
 @Component({
   selector: 'app-ping',
   templateUrl: './ping.component.html',
   styleUrls: ['./ping.component.css']
 })
-export class PingComponent implements OnInit {
+export class PingComponent {
 
-  constructor() { }
+  message: string = '';
 
-  ngOnInit() {
+  constructor(private auth: AuthService, private http: Http, private authHttp: AuthHttp) { }
+
+  public ping() {
+    this.message = '';
+    this.http.get(`${environment.apiUrl}/ping`)
+      .map(res => res.json())
+      .subscribe(
+      data => this.message = data.text,
+      error => this.message = error.toString()
+      );
   }
 
+  public securedPing() {
+    this.message = '';
+    this.authHttp.get(`${environment.apiUrl}/secured/ping`)
+      .map(res => res.json())
+      .subscribe(
+      data => this.message = data.text,
+      error => this.message = error.toString()
+      );
+  }
 }
